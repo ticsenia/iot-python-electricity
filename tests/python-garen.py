ffcf59248481eda51dcca72d7456efab9994df55
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2017-05-19 08:02:44 +0200

    Updated

--- tests/python-garen.py
+++ tests/python-garen.py
@@ -1,57 +1,81 @@
 #!/usr/bin/python3
+#
+#
+# Este script hace:
+# --
+# -- RELLENAR AQUI 
+#
 
 import json
+import requests
 import sys
 import os
 
+
+# Algunas variables que se utilizan en 
+# en Garen
+confDir="conf.d/"
 rutaJSON=""
 rutaNotesJSON=""
 
+
 def die(msg):
 	print(" * [ ERROR ] : "+msg)
-	print(" * [ USAGE ] :  python-garen.py PATH_TO_JSONVALUES PATH_TO_JSONNOTES -c")
+	print(" * [ USAGE ] :  python-garen.py URL [MODELO] -c")
 	sys.exit(1)
 
-def do_csv():
-	print(" * [ CSV Creation ] ")
-	rutaJSON=sys.argv[1]
-	print(" * [ CSV ] : Working with "+rutaJSON)
-	data_file = open(rutaJSON)
-	data = json.load(data_file)
+
+def lastreplace(s,old,new,ocurrence):
+	li = s.rsplit(old, ocurrence)
+	return new.join(li)
 
 
-	# Testing with name definitions
-	print(" * [ CSV ] : Notes definition")
-	rutaNotesJSON=sys.argv[2]
-	notes_file = open(rutaNotesJSON)
-	notes_data = json.load(notes_file)
+def get_list_of_variables(model):
+	# Vamos a generar una variable que contendra el listado de las variables de la Janitza-511
+	# de tal manera el codigo quede lo mas limpio posible
+	if(model == "Janitza511"):
+		conffile=open(confDir+model+".conf")
+		confstring=conffile.read().replace("\n",",")
+		confstring=lastreplace(confstring,","," ",1)
+		return confstring
 
-	csv_file=open("temporal_csvfile.csv","w")
-	csv_file.write("Variable,Medida,Unidad,Descripcion")
-	csv_file.write("\n")
+
+def get_json(url,model):
+	print(" * [ JSON ] : Getting values from URL ")
+
+	var_model=get_list_of_variables(model)
+	url="http://"+url+"/json.do?"+var_model
+
+	r = requests.get(url)
+	return json.load(r)
 	
-	for key, value in data.items():
+
+def do_csv(jmeasures,model):
+	
+	# Testing with name definitions
+	fnotes = open(confDir+model+".definition")
+	jnotes = json.load(fnotes)
+
+	cmeasures=open("temporal_csvfile.csv","w")
+	cmeasures.write("Variable,Medida,Unidad,Descripcion")
+	cmeasures.write("\n")
+	
+	for key, value in jmeasures.items():
 		print("* Key -> " + str(key) + "\n\t* Values -> "+str(value))
 		linea = str(key)+","+str(value[0])+","+str(value[1])+","
 		try:
-			print("\t* Notes -> "+notes_data[key])
-			linea= linea +str(notes_data[key]).replace(",","-").replace(";","-")
+			print("\t* Notes -> "+jnotes[key])
+			linea= linea +str(jnotes[key]).replace(",","-").replace(";","-")
 		except Exception as e:
 			print("\t* Notes -> Vacio")
 			linea= linea +"None"
-		csv_file.write(linea)
-		csv_file.write("\n")
-	
-def do_sanity_checks():
-	if len(sys.argv) != 4:
-		die(" Incorrect number of parameters")
-	
-	if not os.path.exists(sys.argv[1]):
-		die(" The first argument must be a JSON File")
+		cmeasures.write(linea)
+		cmeasures.write("\n")
 
-	if not os.path.exists(sys.argv[2]):
-		die(" The second argument must be a JSON File with notes definitions")
-	
+
+def do_sanity_checks():
+	if len(sys.argv) != 3:
+		die(" Incorrect number of parameters")
 	
 
 #
@@ -60,7 +84,17 @@ def do_sanity_checks():
 
 if __name__ == "__main__":
 	do_sanity_checks()
-	do_csv()
+
+	# Tal vez aqui si os sentis con ganas....
+	model="Janitza511"
+	variable_list = get_list_of_variables(model)
+	
+	# Debug only
+	#jmeasures = get_json(sys.argv[1],model)
+	auxf = open("last.json")
+	jmeasures = json.load(auxf)
+
+	do_csv(jmeasures,model)
 
 
 
